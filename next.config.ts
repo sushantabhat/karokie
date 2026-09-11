ba4f6228cf338895b21f0f733c670df94730bded
sushant <bhattaraisushant99@gmail.com>
2026-09-11 11:10:03 +0545

    feat: configure Docker containerization with standalone output mode

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  output: 'standalone',
 };
 
 export default nextConfig;
